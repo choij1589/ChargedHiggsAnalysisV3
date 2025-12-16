3443d069ca2ebe4ec16ef137603af22b9af117c5
Jin Choi <choij@cern.ch>
2025-12-16 14:50:36 +0900

    preparing AN-25-154 v3

--- DiLepton/doThis.sh
+++ DiLepton/doThis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ERAs=("2016preVFP" "2016postVFP" "2017" "2018" "2022" "2022EE" "2023" "2023BPix" "Run2" "Run3")
-CHANNELs=("DIMU", "EMU")
+ERAs=("2016preVFP" "2016postVFP" "2017" "2018" "Run2")
+CHANNELs=("DIMU" "EMU")
 
 for ERA in "${ERAs[@]}"; do
   for CHANNEL in "${CHANNELs[@]}"; do
